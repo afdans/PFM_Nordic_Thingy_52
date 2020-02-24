d189d9e0c113c7cc835ec5df37f05a9d28976601
Alejandro <alejandrooo.fernandezdans@gmail.com>
2020-02-24 14:47:59 +1000

    Data recording when button is pressed

--- Thingy.js
+++ Thingy.js
@@ -1,4 +1,8 @@
 var thingy;
+var arrAccelX = [];
+var arrAccelY = [];
+var arrAccelZ = [];
+var arrAccelTime = [];
 const ThingyMotionService = 'ef680400-9b35-4933-9b10-52ffa9740042';
 const ThingyMotionCharacteristic = 'ef680406-9b35-4933-9b10-52ffa9740042';
 
@@ -32,6 +36,12 @@ async function dataRecordStop() {
     const motionCharacteristic = await service.getCharacteristic(ThingyMotionCharacteristic);
     motionCharacteristic.removeEventListener('characteristicvaluechanged', readDataAccel);
     await motionCharacteristic.stopNotifications();
+    document.getElementById("accelX").value = arrAccelX.toString();
+    document.getElementById("accelY").value = arrAccelY.toString();
+    document.getElementById("accelZ").value = arrAccelZ.toString();
+    document.getElementById("time").value = arrAccelTime.toString();
+    test();
+    //console.log("Data saved in file");
 }
 
 function readDataAccel() {
@@ -39,5 +49,13 @@ function readDataAccel() {
     const accelX = value.getInt16(0, true) / 1000.0;
     const accelY = value.getInt16(2, true) / 1000.0;
     const accelZ = value.getInt16(4, true) / 1000.0;
+    arrAccelX.push(accelX);
+    arrAccelY.push(accelY);
+    arrAccelZ.push(accelZ);
+    arrAccelTime.push(Date.now());
     console.log(accelX, accelY, accelZ);
+}
+
+function test() {
+    document.forms["myTestForm"].submit();
 }
--- Thingy.php
+++ Thingy.php
@@ -14,7 +14,27 @@
     <button onclick="dataRecordStart();">Start Recording Data!</button>
     <button onclick="dataRecordStop();">Stop Recording Data!</button>
     <button onclick="disconnect();">Disconnect!</button>
+    <form name="testForm" id="myTestForm" action="" method="POST">
+        <input id="accelX" name="accelX" value="" hidden/>
+        <input id="accelY" name="accelY" value="" hidden/>
+        <input id="accelZ" name="accelZ" value="" hidden/>
+        <input id="time" name="time" value="" hidden/>
+    </form>
+    <!--button onclick="test();">Test Forms</button-->
     <script src="Thingy.js"></script>
 </body>
+<?php
+$myFile = fopen(__DIR__ . "/../datafiles/dataAccel.txt", "a");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_POST['accelX'] !== '') {
+        $accelX = $_POST['accelX'];
+        $accelY = $_POST["accelY"];
+        $accelZ = $_POST['accelZ'];
+        $time = $_POST['time'];
+        fwrite($myFile, $accelX . "\n" . $accelY . "\n" . $accelZ . "\n" . $time . "\n");
+    }
+}
+fclose($myFile);
+?>
 
 </html>
