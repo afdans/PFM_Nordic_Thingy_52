3ab9318b431b5bb5983aad557d66150e8edead39
Alejandro <alejandrooo.fernandezdans@gmail.com>
2020-02-25 12:49:44 +1000

    Create tag file to be used by php files

---
+++ TagIds.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * HTML Tag IDs and names for the input tags used
+ * for the  post form
+ */
+ // Environment
+define("TEMPERATURE_ID", "temp");
+define("TEMPERATURE_TIME_ID", "tempT");
+define("PRESSURE_ID", "pressure");
+define("PRESSURE_TIME_ID", "pressureT");
+define("HUMIDITY_ID", "humidity");
+define("HUMIDITY_TIME_ID", "humidityT");
+define("CO2_ID", "CO2");
+define("TVOC_ID", "TVOC");
+define("GAS_TIME_ID", "GasT");
+
+// Motion
+define("ACCELERATION_X_ID", 'accelX');
+define("ACCELERATION_Y_ID", 'accelX');
+define("ACCELERATION_Z_ID", 'accelX');
+define("GYROSCOPE_X_ID", 'gyroX');
+define("GYROSCOPE_Y_ID", 'gyroY');
+define("GYROSCOPE_Z_ID", 'gyroZ');
+define("MOTION_TIME_ID", 'motionT');
