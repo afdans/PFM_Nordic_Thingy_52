0f6dd5a718a899674fd92b21abdaf14483487bec
Alejandro <alejandrooo.fernandezdans@gmail.com>
2020-02-25 13:49:58 +1000

    Use of helper.php in the main page

--- Thingy.php
+++ Thingy.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<?php include'TagIds.php'?>
+<?php include 'TagIds.php'; ?>
+<?php include 'Helper.php'; ?>
 <html lang="en">
 
 <head>
@@ -16,22 +17,9 @@
     <button onclick="dataRecordStop();">Stop Recording Data!</button>
     <button onclick="disconnect();">Disconnect!</button>
     <form name="testForm" id="myTestForm" action="DataSave.php" method="post">
-        <input id="temp" name="temp" value="" hidden />
-        <input id="tempT" name="tempT" value="" hidden />
-        <input id="pressure" name="pressure" value="" hidden />
-        <input id="pressureT" name="pressureT" value="" hidden />
-        <input id="humidity" name="humidity" value="" hidden />
-        <input id="humidityT" name="humidityT" value="" hidden />
-        <input id="CO2" name="CO2" value="" hidden />
-        <input id="TVOC" name="TVOC" value="" hidden />
-        <input id="GasT" name="GasT" value="" hidden />
-        <input id="accelX" name="accelX" value="" hidden />
-        <input id="accelY" name="accelY" value="" hidden />
-        <input id="accelZ" name="accelZ" value="" hidden />
-        <input id="gyroX" name="gyroX" value="" hidden />
-        <input id="gyroY" name="gyroY" value="" hidden />
-        <input id="gyroZ" name="gyroZ" value="" hidden />
-        <input id="motionT" name="motionT" value="" hidden />
+        <?php
+        createTagFromArray(get_defined_constants(true)['user']);
+        ?>
     </form>
     <script src="Thingy.js"></script>
 </body>
