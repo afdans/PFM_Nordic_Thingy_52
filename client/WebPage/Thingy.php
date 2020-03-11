41a249d87b78493fbffb8c419bead7c9c8ac5844
Alejandro <alejandrooo.fernandezdans@gmail.com>
2020-03-11 15:26:25 +1000

    CSS added

--- client/WebPage/Thingy.php
+++ client/WebPage/Thingy.php
@@ -4,10 +4,11 @@
 <html lang="en">
 
 <head>
+    <link rel="stylesheet" type="text/css" href="Thingy.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ei=edge">
-    <title>Document</title>
+    <title>Thingy 52</title>
 </head>
 
 <body>
@@ -22,36 +23,41 @@
         ?>
     </form>
     <button onclick="showConfigs();" id="ConfigBTN">Show Configurations</button>
-    <div id="Configs" style="display:none">
-        <h2>Environment</h2>
-        <p>Temperature: (100 - 60,000 ms)<br>
-            <input type="text" id="temperatureInterval">
-        </p>
-        <p>Pressure: (100 - 60,000 ms)<br>
-            <input type="text" id="pressureInterval">
-        </p>
-        <p>Humidity: (100 - 60,000 ms)<br>
-            <input type="text" id="humidityInterval">
-        </p>
-        <p>Gas:
-            <select id="gasInterval">
-                <option value="1">1 second</option>
-                <option value="2">10 seconds</option>
-                <option value="3">60 seconds</option>
-            </select>
-        </p>
-        <p>Light: (100 - 60,000 ms)<br>
-            <input type="text" id="colorInterval">
-        </p>
-        <p>Color calibration Red (intensity): <br>
-            <input type="text" id="colorCalRed">
-        </p>
-        <p>Color calibration Green (intensity): <br>
-            <input type="text" id="colorCalGreen">
-        </p>
-        <p>Color calibration Blue (intensity): <br>
-            <input type="text" id="colorCalBlue">
-        </p>
+    <div id="Configs" style="display:none" class="row">
+        <div class="column">
+            <h2>Environment</h2>
+            <p>Temperature: (100 - 60,000 ms)<br>
+                <input type="text" id="temperatureInterval">
+            </p>
+            <p>Pressure: (100 - 60,000 ms)<br>
+                <input type="text" id="pressureInterval">
+            </p>
+            <p>Humidity: (100 - 60,000 ms)<br>
+                <input type="text" id="humidityInterval">
+            </p>
+            <p>Gas:
+                <select id="gasInterval">
+                    <option value="1">1 second</option>
+                    <option value="2">10 seconds</option>
+                    <option value="3">60 seconds</option>
+                </select>
+            </p>
+            <p>Light: (100 - 60,000 ms)<br>
+                <input type="text" id="colorInterval">
+            </p>
+            <p>Color calibration Red (intensity): <br>
+                <input type="text" id="colorCalRed">
+            </p>
+            <p>Color calibration Green (intensity): <br>
+                <input type="text" id="colorCalGreen">
+            </p>
+            <p>Color calibration Blue (intensity): <br>
+                <input type="text" id="colorCalBlue">
+            </p>
+        </div>
+        <div class="column">
+            <h2>Motion</h2>
+        </div>
     </div>
     <script src="Thingy.js"></script>
 </body>
