edb77492256e24d22ddce4c9fb0eb8e84f8a1560
Alejandro <alejandrooo.fernandezdans@gmail.com>
2020-03-12 12:58:06 +1000

    Added function that creates html toggle switches

--- client/WebPage/Helper.php
+++ client/WebPage/Helper.php
@@ -9,3 +9,7 @@ function createTagFromArray($nameArray){
 function createTag($name){
     echo "<input id=\"$name\" name=\"$name\" value=\"\" hidden />";
 }
+
+function createToggleSwitch($id){
+    echo "<label class=\"switch\"><input type=\"checkbox\" id=\"$id\" checked><span class=\"slider round\"></span></label><br>";
+}
