47f1d7550541b6e758caa2392979ae1e7edcfef1
Alejandro <alejandrooo.fernandezdans@gmail.com>
2020-03-12 16:02:33 +1000

    Added Acceleration plot in Matlab script

--- client/Matlab/ThingyDataRead.m
+++ client/Matlab/ThingyDataRead.m
@@ -2,9 +2,19 @@ clear all;
 close all;
 clc;
 %%
-addpath('C:\xampp\htdocs\datafiles');
+addpath('C:\xampp\htdocs\PFM_Nordic_Thingy_52\client\datafiles');
 
 motionValues = importdata('dataMotion.csv');
 environmentValues = importdata('dataEnvironment.csv');
 motionData = motionValues.data;
 environmentData = environmentValues.data;
+
+%%
+
+Acc_X = motionData(1, :);
+Acc_Y = motionData(2, :);
+Acc_Z = motionData(3, :);
+
+%%
+figure();
+plot(sqrt(Acc_X .^ 2 + Acc_Y .^ 2 + Acc_Z .^ 2));
